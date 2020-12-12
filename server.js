0bbcc27aebf5150f277140f63b925ef2fed881be
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-12-12 23:26:56 +0900

    use express.json

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const connectDB = require('./config/db');
 
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 app.use('/auth', require('./route/authRouter'));
 app.use('/user', require('./route/userRouter'));
 app.use('/profile', require('./route/profileRouter'));
 app.use('/post', require('./route/postRouter'));
-
-app.get('/', (req, res) => {
-  res.json("it's working!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server starts on ${PORT}`);
