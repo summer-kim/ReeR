52585b59f6c24d5e98c39ba1deac5a5c03229bff
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-12-12 23:25:59 +0900

    api user register

--- route/userRouter.js
+++ route/userRouter.js
@@ -1,6 +1,78 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../model/userModel');
+
+// POST '/user/register'
+// register
+// Public
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please provide valid email').isEmail(),
+    check('password', 'Provide password at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5days' },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// GET '/user'
+// Test
+// Public
+router.get('', (req, res) => {
+  res.json('user router');
+});
+
 // GET '/user'
 // Test
 // Public
