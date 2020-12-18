904fc19ea0e5067ddc2f40149e1af3fc5b9c4bc4
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-12-19 00:37:49 +0900

    Navbar is seperated for loginUser and guestUser

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
+const Navbar = ({ authReducer: { isAuthenticated, loading } }) => {
+  const authLink = (
+    <Link to='/mypage'>
+      <i className='far fa-user'></i>
+    </Link>
+  );
+  const guestLink = (
+    <Link to='/login'>
+      Register
+      <br />
+      /Login
+    </Link>
+  );
   return (
-    <nav id='nav' class='flex-container'>
-      <a id='nav-a' href='./tags.html'>
-        <i class='fas fa-tags'></i>Tags
-      </a>
+    <nav id='nav' className='flex-container'>
+      <Link id='nav-a' to='/tags'>
+        <i className='fas fa-tags'></i>Tags
+      </Link>
       <div id='nav-b'>
         <Link to='/'>
-          <span class='logo'>ReeR:</span>
+          <span className='logo'>ReeR:</span>
         </Link>
       </div>
-      <div id='nav-c' class='flex-container'>
-        <a href='./about.html'>About</a>
-        <Link to='/login'>
-          Register
-          <br />
-          /Login
-        </Link>
-        <a href='./mypage.html'>
-          <i class='far fa-user'></i>
-        </a>
+      <div id='nav-c' className='flex-container'>
+        <Link to='/about'>About</Link>
+        {!loading && isAuthenticated ? authLink : guestLink}
       </div>
     </nav>
   );
 };
-export default Navbar;
+Navbar.propTypes = {
+  //logout: PropTypes.func.isRequired,
+  authReducer: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  authReducer: state.authReducer,
+});
+export default connect(mapStateToProps)(Navbar);
