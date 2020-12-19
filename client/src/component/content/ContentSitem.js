209612b0ea94fc5a8281d3638b0f2055ee77cb63
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-12-19 18:28:11 +0900

    put Reducer and Like-event-handler

--- client/src/component/content/ContentSitem.js
+++ client/src/component/content/ContentSitem.js
@@ -2,41 +2,114 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import lala from '../../img/lala.jpg';
+import PropTypes from 'prop-types';
+import { likePost, likePostUndo } from '../../action/postAction';
 
-const ContentSitem = () => {
+const ContentSitem = ({
+  content: {
+    _id,
+    genre, //array
+    movieName,
+    img,
+    user, //objectID
+    likes,
+    unlikes,
+    tags,
+  },
+  likePost,
+  likePostUndo,
+  authReducer,
+}) => {
+  let addClassLiked = false;
+  // const onClickLike = (e) => {
+  //   likePost(_id);
+  //   if (Object.keys(error).length === 0) {
+  //     toggle(!addClassInput);
+  //   }
+  // };
+  if (
+    authReducer.user &&
+    likes.some((like) => like.user === authReducer.user._id)
+  ) {
+    addClassLiked = true;
+  }
+  const onClickLike = (e) => {
+    if (addClassLiked) {
+      likePostUndo(_id);
+      addClassLiked = false;
+    } else {
+      likePost(_id);
+      addClassLiked = true;
+    }
+  };
   return (
-    <Link to='/content'>
-      <div className='item'>
-        <div className='item-img'>
+    <div className='item'>
+      <div className='item-img'>
+        <Link to='/content'>
           <img src={lala} alt='#' />
-          <div className='item-text'>
-            <div className='emoji emoji-heart'>
-              <i className='fas fa-heart'></i>
-            </div>
-            <div className='emoji emoji-broken'>
-              <i className='fas fa-heart-broken'></i>
-            </div>
-            <div className='emoji emoji-plus'>
-              <i className='fas fa-plus'></i>
-            </div>
+        </Link>
+        <div className='item-text'>
+          <div
+            onClick={onClickLike}
+            className={
+              addClassLiked
+                ? 'emoji emoji-heart emoji-reverse'
+                : 'emoji emoji-heart'
+            }
+          >
+            <i className='fas fa-heart'></i>
+          </div>
+          <div className='emoji emoji-broken'>
+            <i className='fas fa-heart-broken'></i>
+          </div>
+          <div className='emoji emoji-plus'>
+            <i className='fas fa-plus'></i>
           </div>
         </div>
-        <div>
-          <span className='movieTitle'>Gone with wind</span>
-        </div>
-        <div className='info'>
-          <span className='genre'> Adventure/SF </span>
-          <span className='interest'>
-            <i className='fas fa-heart'></i>98
-          </span>
-        </div>
-        <div className='tags'>
-          <span className='tag'>Adventureous</span>
-          <span className='tag'>Good to watch at night</span>
-          <span className='tag'>Teach me the lessons of life</span>
-        </div>
       </div>
-    </Link>
+      <div>
+        <span className='movieTitle'>{movieName}</span>
+      </div>
+      <div className='info'>
+        <span>
+          {genre.map((gen, index) => {
+            if (index === genre.length - 1) {
+              return (
+                <span key={index} className='genre'>
+                  {gen.toUpperCase()}
+                </span>
+              );
+            } else {
+              return (
+                <span key={index} className='genre'>
+                  {gen.toUpperCase()}/
+                </span>
+              );
+            }
+          })}
+        </span>
+        <span className='interest'>
+          <i className='fas fa-heart'></i>
+          {likes.length}
+        </span>
+      </div>
+      <div className='tags'>
+        <span className='tag'>Adventureous</span>
+        <span className='tag'>Good to watch at night</span>
+        <span className='tag'>Teach me the lessons of life</span>
+      </div>
+    </div>
   );
 };
-export default ContentSitem;
+ContentSitem.propTypes = {
+  content: PropTypes.object.isRequired,
+  likePost: PropTypes.func.isRequired,
+  likePostUndo: PropTypes.func.isRequired,
+  authReducer: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  authReducer: state.authReducer,
+});
+export default connect(mapStateToProps, { likePost, likePostUndo })(
+  ContentSitem
+);
