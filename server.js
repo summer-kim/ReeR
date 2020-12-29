909f127a797cd00311de47752f76195435414718
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-12-30 00:45:54 +0900

    testing heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
@@ -13,6 +13,16 @@ app.use('/user', require('./route/userRouter'));
 app.use('/profile', require('./route/profileRouter'));
 app.use('/post', require('./route/postRouter'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server starts on ${PORT}`);
 });
