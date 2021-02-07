c2d51080405b12411c865cdd20081870af56e694
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-02-08 02:46:50 +0900

    integrate Login and Register

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,61 +1,97 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { loginUser } from '../../redux/action/authAction';
+import { loginUser, registerUser } from '../../redux/action/authAction';
+import { setAlert } from '../../redux/action/alertAction';
 
 const Login = ({ loginUser, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
+  const [isLoginPage, setIsLoginPage] = useState(true);
+
+  const [loginData, setLoginData] = useState({
     email: '',
     password: '',
   });
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
 
-  const { email, password } = formData;
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    isLoginPage
+      ? setLoginData({ ...loginData, [e.target.name]: e.target.value })
+      : setRegisterData({ ...registerData, [e.target.name]: e.target.value });
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    loginUser({ email, password });
+    if (isLoginPage) {
+      loginUser(loginData);
+      return;
+    }
+
+    const { name, email, password, password2 } = registerData;
+
+    if (password !== password2) {
+      setAlert('Password does not match', 'fail');
+    } else {
+      registerUser({ name, email, password });
+    }
   };
+
   if (isAuthenticated) {
     return <Redirect to='/' />;
   }
+
   return (
     <section id='login'>
       <div id='login-box' className='flex-container'>
         <div className='intro'>
-          <h2>sign in</h2>
+          <h2>{isLoginPage ? 'Login' : 'Register'}</h2>
           <div className='bottom-line'></div>
           <div className='loginLink'>
-            <span>Not a member yet?</span>
-            <Link className='btn-main' to='/register'>
-              Register
-            </Link>
+            <span>
+              {isLoginPage ? 'Not a member yet?' : 'Already our member?'}
+            </span>
+            <div
+              className='btn-main'
+              onClick={() => setIsLoginPage(!isLoginPage)}
+            >
+              {isLoginPage ? 'Register' : 'Login'}
+            </div>
           </div>
         </div>
         <div className='form'>
           <form onSubmit={onSubmit}>
-            <div className='eachForm'>
-              <label htmlFor='email'>Email</label>
-              <input
-                type='email'
-                name='email'
-                onChange={onChange}
-                value={email}
-              />
-            </div>
-            <div className='eachForm'>
-              <label htmlFor='password'>Password</label>
-              <input
-                type='password'
-                name='password'
-                onChange={onChange}
-                value={password}
-              />
-            </div>
+            {isLoginPage
+              ? Object.keys(loginData).map((title, idx) => (
+                  <div className='eachForm' key={idx}>
+                    <label htmlFor={title}>{title}</label>
+                    <input
+                      type={title}
+                      name={title}
+                      onChange={onChange}
+                      value={Object.values(loginData)[idx]}
+                    />
+                  </div>
+                ))
+              : Object.keys(registerData).map((title, idx) => (
+                  <div className='eachForm' key={idx}>
+                    <label htmlFor={title}>{title}</label>
+                    <input
+                      type={title.includes('password') ? 'password' : title}
+                      name={title}
+                      onChange={onChange}
+                      value={Object.values(registerData)[idx]}
+                    />
+                  </div>
+                ))}
+
             <button className='btn-main' type='submit'>
-              Sign in
+              {isLoginPage ? 'Sign in' : 'Sign up'}
             </button>
           </form>
         </div>
