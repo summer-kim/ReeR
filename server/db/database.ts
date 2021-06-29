d790fdfa6ea4b2ec0fb77b143183462a8148ff04
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-06-29 17:48:44 +0900

    rollback to previous version

--- server/db/database.ts
+++ server/db/database.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
 import { config } from '../../config';
 
-const { host, username, database, password } = config.SQL;
+const { host, port, username, database, password } = config.SQL;
 export const sequelize = new Sequelize(database, username, password, {
   host,
+  port: Number(port),
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
